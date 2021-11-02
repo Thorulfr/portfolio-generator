f01cec57a2aaf83b12cbf2660010795a274f99a1
Ben H <deepfreespace@gmail.com>
2021-11-02 16:39:43 -0600

    Installed Inquirer via NPM

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+/* const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-fs.writeFile('index.html', generatePage(name, github), err => {
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('index.html', pageHTML, err => {
     if (err) throw (err);
+
     console.log('Portfolio complete! Check out index.html to see the output!');
-});
+}); */
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
